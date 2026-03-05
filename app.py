81385bf0e58f17a3b4133e866e078351be329bae
leonido3 <leonido3@nycstudents.net>
2026-03-05 09:26:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,21 @@ def csv_to_list(file_path):
             row = line.strip().split(',')
             row = [int(value) if value.isdigit() else value for value in row]
             data_list.append(row)
+    return data_list
 
 
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
-print(data)
+def avgcalc():
+    global avgdata
+    avgdata = []
+    sumrow = 0
+    for i in range(10):
+        for x in range(30):
+            sumrow += data[i+1][x+1]
+        sumrow = sumrow/30
+        avgdata.append(sumrow)
+    for index, item in enumerate(avgdata):
+        print(data[index][0], ": $", round(item))
+def avgsort():
+    sorted_data = avgdata.sort()
