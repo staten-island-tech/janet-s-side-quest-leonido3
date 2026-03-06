3260602618ceb8a291edca6e7584c0a4388cb50c
leonido3 <leonido3@nycstudents.net>
2026-03-06 09:26:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,31 @@ def csv_to_list(file_path):
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
 def avgcalc():
-    global avgdata
+    global full_data, avgdata
     avgdata = []
     sumrow = 0
     for i in range(10):
         for x in range(30):
             sumrow += data[i+1][x+1]
         sumrow = sumrow/30
-        avgdata.append(sumrow)
+        avgdata.append(round(sumrow))
+    full_data = []
     for index, item in enumerate(avgdata):
-        print(data[index][0], ": $", round(item))
-def avgsort():
-    sorted_data = avgdata.sort()
+        full_data.append((f"{data[index+1][0]}", item))
+    print(full_data)
+avgcalc()
+def sort_data():
+    sorted_data = []
+    numsort = sorted(avgdata)
+    for i in range(len(numsort)):
+        for x in range(len(full_data)):
+            if numsort[i] == full_data[x][1]:
+                sorted_data.append((full_data[x][0], numsort[i]))
+    print(sorted_data)
+sort_data()
+def allavg():
+    avgdatasum = sum(avgdata)
+    fullavg = avgdatasum/len(avgdata)
+    print(f"Full average: {fullavg}")
+allavg()
+def underperf():
